4533c2f8bd7f3f713734dc662abe7e276097eb7f
pavitrapatil2017 <pavikp4@gmail.com>
2023-03-28 13:13:53 +0530

    adding all these files

---
+++ Employee.java
@@ -0,0 +1,23 @@
+class Person
+{
+public static void main(String[] ar)
+{
+System.out.println("Hello.....");
+}
+}
+public class Employee
+{
+public static void main(String[] ar)
+{
+System.out.println("Hello....Employee.");
+int number=100;
+char grade='A';
+boolean status=true;
+float marks=90.78f;
+System.out.println("Number is-->  "+number);
+System.out.println("The grade is---> "+grade);
+System.out.println("The status is---> "+status);
+System.out.println("The marks is---> "+marks);
+
+}
+}
---
+++ Format.java
@@ -0,0 +1,9 @@
+public class Format
+{
+	public static void main(String args[])
+	{
+		System.out.println("                              "+"Hello");
+		System.out.println("            "+"welcome"+"         "+"to");
+		System.out.println("   "+"edubridge"+"    "+"online"+"            "+"class");
+	}
+}
